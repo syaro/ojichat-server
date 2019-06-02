7fdf2dade5d8ef593893c49744ce9bedddba08b5
Yuji Kurihara <syaro@gochiusa.net>
2019-06-03 00:37:55 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/syaro/ojichat-server
+
+go 1.12
+
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	github.com/gin-contrib/cors v1.3.0
+	github.com/gin-gonic/gin v1.4.0
+	github.com/greymd/ojichat v0.1.0
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5 // indirect
+	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
+	golang.org/x/sys v0.0.0-20190602015325-4c4f7f33c9ed // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190602112858-2de7f9bf822c // indirect
+	google.golang.org/appengine v1.6.0
+)
---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/greymd/ojichat/generator"
+	"google.golang.org/appengine"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	router.POST("/post", createMessage)
+
+	http.Handle("/", router)
+	appengine.Main()
+}
+
+func createMessage(c *gin.Context) {
+	name := c.PostForm("name")
+	emojiLevel := c.DefaultPostForm("emoji_level", "4")
+	punctiuationLebel := c.DefaultPostForm("punctiuation_level", "1")
+
+	if num, err := strconv.Atoi(emojiLevel); err != nil || num > 10 {
+		emojiLevel = "4"
+	}
+
+	config := generator.Config{}
+	config.TargetName = name
+	config.EmojiNum, _ = strconv.Atoi(emojiLevel)
+	config.PunctiuationLebel, _ = strconv.Atoi(punctiuationLebel)
+
+	result, err := generator.Start(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(200, gin.H{
+		"name":               name,
+		"emoji_level":        emojiLevel,
+		"punctiuation_level": punctiuationLebel,
+		"message":            result,
+	})
+}
