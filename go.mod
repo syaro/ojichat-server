abd3e1062211e1aeca3cfc7fc9a7e00fe2b97994
Yuji Kurihara <syaro@gochiusa.net>
2019-06-03 20:36:29 +0900

    fix: typo?

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
-	github.com/greymd/ojichat v0.1.0
+	github.com/greymd/ojichat v0.2.0
 	github.com/mattn/go-isatty v0.0.8 // indirect
 	github.com/stretchr/objx v0.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5 // indirect
--- server.go
+++ server.go
@@ -24,16 +24,21 @@ func main() {
 func createMessage(c *gin.Context) {
 	name := c.PostForm("name")
 	emojiLevel := c.DefaultPostForm("emoji_level", "4")
-	punctiuationLevel := c.DefaultPostForm("punctiuation_level", "1")
+	punctuationLevel := c.DefaultPostForm("punctuation_level", "1")
 
-	if num, err := strconv.Atoi(emojiLevel); err != nil || num < 1 || num > 10 {
+	if num, err := strconv.Atoi(emojiLevel); err != nil || 0 > num || num > 10 {
 		emojiLevel = "4"
+	}
+
+	if num, err := strconv.Atoi(punctuationLevel); err != nil || 0 > num || num > 3 {
+		punctuationLevel = "0"
 	}
 
 	config := generator.Config{}
 	config.TargetName = name
 	config.EmojiNum, _ = strconv.Atoi(emojiLevel)
-	config.PunctiuationLebel, _ = strconv.Atoi(punctiuationLevel)
+	// Typo? Punctiuation -> PunctuationLevel
+	config.PunctiuationLebel, _ = strconv.Atoi(punctuationLevel)
 
 	result, err := generator.Start(config)
 	if err != nil {
@@ -41,9 +46,9 @@ func createMessage(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{
-		"name":               name,
-		"emoji_level":        emojiLevel,
-		"punctiuation_level": punctiuationLevel,
-		"message":            result,
+		"name":              name,
+		"emoji_level":       emojiLevel,
+		"punctuation_level": punctuationLevel,
+		"message":           result,
 	})
 }
